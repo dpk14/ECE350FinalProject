dc43f91e5c7fad77d932cf23cd8aedbe16ca1942
JoshR220 <josh.rabinowitz1@gmail.com>
2021-11-22 10:50:08 -0500

    assembly revisions

--- Assembly_Files/game_code.s
+++ Assembly_Files/game_code.s
@@ -1,14 +1,13 @@
 init: 
 #initialize score 
-addi $r26, $r26,0
+#assume bird width to be 20
+#assume bird height to be 10
+addi $r26, $r26, 0
 #bird's x coord
-addi $r23, $r1, 120
-#bird's y coord 
-addi $r24, $r2, 320
-#bird's width
-addi $r22, r22, 20
-#bird's height
-addi $r22, r22, 10
+addi $r6, $r0, 120
+#bird's y coord (top)
+addi $r5, $r0, 325 (320+5)
+
 
 game_loop:
 
@@ -24,13 +23,21 @@ j game_loop
 
 check_collision: 
 #assume pipe left edge coord is stored in reg 1
+addi $r23, $r6, 5
 blt $r23, $r1, cond1_met
 jr $ra
 cond1_met:
-#assume pipe top edge coord is stored in reg 2
-blt $r24, $r2, cond2_met
+#assume pipe left edge coord is stored in reg 1
+addi $r22, $r1, 25
+blt $r23, $r22, cond2_met
 jr $ra
-conda2_met:
+cond2_met:
+#assume pipe top edge coord is stored in reg 2
+add $r21, $r5, -10
+blt $r21, $r2, cond3_met
+jr $ra
+
+cond3_met:
 j end_game
 
 
