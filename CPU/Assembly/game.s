0a96d351a4f0a14360c1a35a96d2619e765cc8b9
JoshR220 <josh.rabinowitz1@gmail.com>
2021-11-28 08:33:01 -0500

    assembly reg updated

--- CPU/Assembly/game.s
+++ CPU/Assembly/game.s
@@ -6,9 +6,21 @@ init:
 #keep high score at memory address 0
 
 
-addi $r19, $r0, 94 #bird's (right) x coord (120+10)
-addi $r20, $r0, 325 #bird's y coord (top- 320+5)
-
+init:
+addi $r1, $r0, 120              #pipe 1 x left edge
+addi $r2, $r0, 190              #pipe 1 y center
+addi $r3, $r0, 100              #pipe 1 y gap height
+addi $r4, $r0, 270              #pipe 2 x left edge
+addi $r5, $r0, 140              #pipe 2 y center
+addi $r6, $r0, 150              #pipe 2 y gap height
+addi $r7, $r0, 420              #pipe 3 x left edge
+addi $r8, $r0, 340              #pipe 3 y center
+addi $r9, $r0, 200              #pipe 3 y gap height
+addi $r10, $r0, 570             #pipe 4 x left edge
+addi $r11, $r0, 240             #pipe 4 y center
+addi $r12, $r0, 150             #pipe 4 y gap height
+addi $r13, $r0, 325              #bird's y coord (top)
+addi $r14, $r0, 94 #bird's (right) x coord (120+10)
 addi $r21, $r0, 10 #r21 scores vertical height gained by bird on jump
 addi $r22, $r0, 1 #r22 store speed of incoming pipe
 addi $r23, $r0,0 #r23 stores how many game rates we've gone through 
@@ -19,13 +31,21 @@ lw $r27, 0($r0) #find high score
 
 game_loop:
 #check if button pressed
-addi $r20, $r20,-1
+addi $r13, $r13,-1
 jal check_collision #check if collision occurred
 bne $r29, $r0, 2 #check if flappy should jump
 bne $r28, $r0, 2 #check if frame should be updated
 j game_loop
 j button_pressed
 j move_pipes #check if need to update screen 
+j check_fall
+
+check_fall:
+addi $r17, $r13, -48 #make $r17 the bottom of the bird
+bne $r17, $r0, 1 #if $r17 is not 0 then do not end game 
+j end_game #end game
+add $r17, $r0, $r0 #clear $r17 
+j game_loop
 
 move_pipes: #update pipe position (may have to tweak addi parameter to see how fast pipe moves)
 addi $r23,$r23,1  #update total count of game frame 
@@ -40,22 +60,22 @@ sub $r28, $r28, $r28 #clear frame rate register
 j game_loop
 
 check_collision:  #just checking pipe 1 for now 
-addi $r18, $r20, -48 #set $r18 to temporarily be bottom edge of bird
+addi $r18, $r13, -48 #set $r18 to temporarily be bottom edge of bird
 add $r17, $r2, $r3, #set $r17 to temporarily be "height" of top pipe
 blt $r18, $r2,2  #branch if bottom edge of bird is below top edge of pipe 
-blt $r17, $r20, 1  #branch if top edge of bird is above top pipe height
+blt $r17, $r13, 1  #branch if top edge of bird is above top pipe height
 j clear_temp_pipes #between pipes can jump back as no collision is possible
 add $r0, $r0, $r0 #collision is possible check left, right and midpoint
-addi $r16, $r19, -17 #find midpoint of bird
-addi $r15, $r19, -34 #find left edge of bird
-addi $r14, $r1, 58 #find right edge of pipes
-blt $r1, $r19, 3  #check if bird right edge is past left point
+addi $r16, $r14, -17 #find midpoint of bird
+addi $r15, $r14, -34 #find left edge of bird
+addi $r19, $r1, 58 #find right edge of pipes
+blt $r1, $r14, 3  #check if bird right edge is past left point
 blt $r1, $r16, 3 #check if bird midpt is past left point
 blt $r1, $r15, 3 #check if bird  left edge is past left point
 j clear_temp_pipes #if none of above conditions met jump back to game loop
-blt  $r19, $r14, 3  #check if bird right edge is before pipe right point
-blt  $r16, $r14, 2 #check if bird right edge is before pipe right point
-blt  $r15, $r14, 1 #check if bird right edge is before pipe right point
+blt  $r14, $r19, 3  #check if bird right edge is before pipe right point
+blt  $r16, $r19, 2 #check if bird right edge is before pipe right point
+blt  $r15, $r19, 1 #check if bird right edge is before pipe right point
 j clear_temp_pipes
 j end_game
 
@@ -64,12 +84,11 @@ sub $r18, $r18, $r18 #clear temp registers
 sub $r17, $r17, $r17
 sub $r16, $r16, $r16
 sub $r15, $r15, $r15
-sub $r14, $r14, $r14
-sub $r13, $r13, $r13 
+sub $r19, $r19, $r19
 j game_loop
 
 button_pressed: #may need to introduce procedure to slowly update value of bird's y_coord
-add $r5, $r5, $r21
+add $r13, $r13, $r21
 sub $r29, $r29, $r29 #clear button register
 j game_loop
 
